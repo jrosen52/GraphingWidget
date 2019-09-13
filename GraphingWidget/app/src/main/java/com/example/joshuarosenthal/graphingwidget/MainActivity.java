3fc6a4ad23b4a6a05542339096315329eaa2666f
Joshua Rosenthal <jrosen52@binghamton.edu>
2019-09-13 10:24:11 -0400

    Added surface

--- GraphingWidget/app/src/main/java/com/example/joshuarosenthal/graphingwidget/MainActivity.java
+++ GraphingWidget/app/src/main/java/com/example/joshuarosenthal/graphingwidget/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.joshuarosenthal.graphingwidget;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
