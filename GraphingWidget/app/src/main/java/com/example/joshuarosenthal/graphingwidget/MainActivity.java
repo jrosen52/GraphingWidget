02bb3bb556912e0607bf801bb47c405aac209480
Joshua Rosenthal <jrosen52@binghamton.edu>
2019-09-18 18:37:34 -0400

    Added touch features

--- GraphingWidget/app/src/main/java/com/example/joshuarosenthal/graphingwidget/MainActivity.java
+++ GraphingWidget/app/src/main/java/com/example/joshuarosenthal/graphingwidget/MainActivity.java
@@ -13,11 +13,78 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+
+    private LinearLayout canvasLayout = null;
+
+    AppSurface customSurfaceView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("SurfaceView");
+
+        initControls();
+
+        // Hide the app title bar.
+        getSupportActionBar().hide();
+
+        // Make app full screen to hide top status bar.
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Create custom surfaceview object.
+        customSurfaceView = new AppSurface(getApplicationContext());
+
+        // Set this as the onTouchListener to process custom surfaceview ontouch event.
+        customSurfaceView.setOnTouchListener(this);
+
+        // Add the custom surfaceview object to the layout.
+        canvasLayout.addView(customSurfaceView);
+
+    }
+
+    /* Initialise ui controls. */
+    private void initControls()
+    {
+        // This layout is used to contain custom surfaceview object.
+        if(canvasLayout == null)
+        {
+            canvasLayout = (LinearLayout)findViewById(R.id.customViewLayout);
+        }
+    }
+
+    /* If user finger touch the surfaceview object. */
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        // If user touch the custom SurfaceView object.
+        if(view instanceof SurfaceView) {
+            view.invalidate();
+
+            float x = motionEvent.getX();
+
+            float y = motionEvent.getY();
+
+            customSurfaceView.setCircleX(x);
+
+            customSurfaceView.setCircleY(y);
+
+            // Create and set a red paint to custom surfaceview.
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);
+            customSurfaceView.setPaint(paint);
+
+            customSurfaceView.drawBall();
+
+
+            // Tell android os the onTouch event has been processed.
+            return true;
+        }else
+        {
+            // Tell android os the onTouch event has not been processed.
+            return false;
+        }
     }
 }
